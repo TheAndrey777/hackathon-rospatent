74769cf1f44317134fe95fc7f1baaa9349697159
Electrominch <64524860+Electrominch@users.noreply.github.com>
2022-09-21 02:00:12 +0300

    Готова навигация

--- RospatentHackathon/App.xaml.cs
+++ RospatentHackathon/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿namespace RospatentHackathon;
+﻿using RospatentHackathon.Views;
+
+namespace RospatentHackathon;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-		MainPage = new NavigationPage(new Views.MyTabbedPage());
+		MainPage = new MyTabbedPage();
 	}
 }
