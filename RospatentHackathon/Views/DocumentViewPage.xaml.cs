9837a265b064d1ff72b6b591ef993b90343f6129
Electrominch <64524860+Electrominch@users.noreply.github.com>
2022-09-22 07:29:13 +0300

    Работает

--- RospatentHackathon/Views/DocumentViewPage.xaml.cs
+++ RospatentHackathon/Views/DocumentViewPage.xaml.cs
@@ -9,70 +9,131 @@ namespace RospatentHackathon.Views;
 public partial class DocumentViewPage : ContentPage
 {
     public Document Document;
+    private Dictionary<string, LangBiblio> biblio => Document.biblio;
     public string searchString;
 
     public DocumentViewPage()
     {
         InitializeComponent();
         Crutch.DocumentView = this;
-        //DownloadDoc("RU2358138C1_20090610");
+        DownloadDoc("RU2358138C1_20090610", "ru");
     }
 
-    public async void DownloadDoc(string id)
+    public async void DownloadDoc(string id, string preferLang)
     {
         Console.WriteLine($"Документ: {id}");
         Document = await HttpApiClient.GetDocument(id);
         if (Document == null) return;
-
-        HTMLPage.Html = @"<HTML><BODY>";
+        string html = @"<HTML><BODY>";
 
         #region  Создание шапки  
-        //HTMLPage.Html += "<h2 align=\"center\">" + Document.biblio.ru.title + "</h2>";
+        html += "<h2 align=\"center\">" + LangForTitle(preferLang)?.title + "</h2>";
 
-        HTMLPage.Html += "<p><b>Номер заявления:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.id + "</p>";
+        html += "<p><b>Номер заявления:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.id + "</p>";
 
-        HTMLPage.Html += "<p><b>Дата подачи заявления:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.common.application.filing_date + "</p>";
+        html += "<p><b>Дата подачи заявления:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.common.application.filing_date + "</p>";
 
-        HTMLPage.Html += "<p><b>Дата публикации:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.common.publication_date + "</p>";
+        html += "<p><b>Дата публикации:</b><br><img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + Document.common.publication_date + "</p>";
 
         string applicants = "";
-        //foreach (var applic in Document.biblio.ru.applicant)
+        foreach (var applic in LangForApplicant(preferLang)?.applicant??new List<Applicant>())
         {
-            //applicants += "<img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + applic.name + ",<br>";
+            applicants += "<img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + applic.name + ",<br>";
         }
         applicants = applicants.Substring(0, Math.Max(applicants.Length - 5, 0));
-        HTMLPage.Html += "<p><b>Заявители:</b><br>" + applicants + "</p>";
+        html += "<p><b>Заявители:</b><br>" + applicants + "</p>";
 
         applicants = "";
-        //foreach (var applic in Document.biblio.ru.inventor)
+        foreach (var invent in LangForInventor(preferLang)?.inventor??new List<Inventor>())
         {
-            //applicants += "<img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + applic.name + ",<br>";
+            applicants += "<img src=\"https://searchplatform.rospatent.gov.ru/0.2.0.552/images/view.svg\">" + invent.name + ",<br>";
         }
         applicants = applicants.Substring(0, Math.Max(applicants.Length - 5, 0));
-        HTMLPage.Html += "<p><b>Авторы:</b><br>" + applicants + "</p>";
+        html += "<p><b>Авторы:</b><br>" + applicants + "</p>";
         #endregion
 
 
-        HTMLPage.Html += "<h3>Реферат</h3>";
-        //HTMLPage.Html += TextAligne(Document.@abstract.ru, "justify");
+        html += "<h3>Реферат</h3>";
+        html += TextAligne(StringForAbstract(preferLang), "justify");
 
         if (Document.drawings != null)
             foreach (var im in Document.drawings)
             {
-                HTMLPage.Html += "<p><img src=\"https://searchplatform.rospatent.gov.ru" + im.url + "\" wigth = \"" + im.width + "\" height = \"" + im.height + "\" ></p>";
+                html += "<p><img src=\"https://searchplatform.rospatent.gov.ru" + im.url + "\" wigth = \"" + im.width + "\" height = \"" + im.height + "\" ></p>";
             }
-        HTMLPage.Html += "<h3>Функция</h3>";
-        //HTMLPage.Html += TextAligne(Document.claims.ru, "justify");
-        HTMLPage.Html += "<h3>Описание</h3>";
-        //HTMLPage.Html += TextAligne(Document.description.ru, "justify");
+        html += "<h3>Функция</h3>";
+        html += TextAligne(StringForClaims(preferLang), "justify");
+        html += "<h3>Описание</h3>";
+        html += TextAligne(StringForDesc(preferLang), "justify");
         if (searchString != null)
         {
-            HTMLPage.Html = HTMLPage.Html.Replace(searchString, "<span style = \"background-color: #ffff00;\" >" + searchString + "</span>");
+            html = html.Replace(searchString, "<span style = \"background-color: #ffff00;\" >" + searchString + "</span>");
         }
-        HTMLPage.Html += "</BODY></HTML>";
+        html += "</BODY></HTML>";
+        HTMLPage.Html = html;
     }
+
+    private LangBiblio LangForTitle(string preferLang)
+    {
+        if (biblio[preferLang].title != null)
+            return biblio[preferLang];
+        foreach (var value in biblio.Values)
+            if (value.title != null)
+                return value;
+        return null;
+    }
+    
+    private LangBiblio LangForApplicant(string preferLang)
+    {
+        if (biblio[preferLang].applicant != null)
+            return biblio[preferLang];
+        foreach (var value in biblio.Values)
+            if (value.applicant != null)
+                return value;
+        return null;
+    }
+    
+    private LangBiblio LangForInventor(string preferLang)
+    {
+        if (biblio[preferLang].inventor != null)
+            return biblio[preferLang];
+        foreach (var value in biblio.Values)
+            if (value.inventor != null)
+                return value;
+        return null;
+    }
+    
+    private string StringForAbstract(string preferLang)
+    {
+        if (Document.@abstract?.ContainsKey(preferLang)??false)
+            return Document.@abstract[preferLang];
+        foreach (var value in Document.@abstract?.Values ?? new Dictionary<string, string>().Values)
+            return value;
+        return null;
+    }
+    
+    private string StringForClaims(string preferLang)
+    {
+        if (Document.claims?.ContainsKey(preferLang)??false)
+            return Document.claims[preferLang];
+        foreach (var value in Document.claims?.Values??new Dictionary<string, string>().Values)
+            return value;
+        return null;
+    }
+    
+    private string StringForDesc(string preferLang)
+    {
+        if (Document.description?.ContainsKey(preferLang)??false)
+            return Document.description[preferLang];
+        foreach (var value in Document.description?.Values ?? new Dictionary<string, string>().Values)
+            return value;
+        return null;
+    }
+
     private string TextAligne(string HTMLText, string type)
     {
+        if (string.IsNullOrEmpty(HTMLText))
+            return "";
         return HTMLText.Replace("<p", "<p align=\"" + type + "\" ");
     }
 }
